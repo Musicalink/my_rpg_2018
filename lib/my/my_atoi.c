2b70ee91d3e386834f02f6723b3fa5331bfac372
Maxence Carpentier <maxence.carpentier@epitech.eu>
2019-04-30 12:57:13 +0200

    [ADD] my_itoa

--- lib/my/my_atoi.c
+++ lib/my/my_atoi.c
@@ -44,3 +44,27 @@ int my_natoi(char *array, int n)
         nb *= -1;
     return (nb);
 }
+
+char *my_itoa(int nb)
+{
+    char *str;
+    int incrementer = 0;
+    int buffer = nb;
+
+    if (nb < 0) {
+        str = malloc(sizeof(char) * my_intlen(nb) + 2);
+        nb *= -1;
+    } else
+        str = malloc(sizeof(char) * my_intlen(nb) + 1);
+    for (int i = 0; nb != 0; i++) {
+        str[incrementer] = (char)(nb % 10 + '0');
+        nb /= 10;
+        incrementer++;
+    }
+    if (buffer < 0) {
+        str[incrementer] = '-';
+        incrementer++;
+    }
+    str[incrementer] = '\0';
+    return (my_revstr(str));
+}
