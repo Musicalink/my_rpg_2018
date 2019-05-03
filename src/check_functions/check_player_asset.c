76a42db1417dc2536785ad9208e4e11649482d56
Maxence Carpentier <maxence.carpentier@epitech.eu>
2019-05-03 17:21:43 +0200

    [DEL] Deleting useless func

--- src/check_functions/check_player_asset.c
+++ src/check_functions/check_player_asset.c
@@ -7,7 +7,6 @@
 
 #include <stddef.h>
 #include "rpg.h"
-#include "struct.h"
 
 int check_sprite_texture(sfSprite *sprite, sfTexture *texture)
 {
@@ -17,26 +16,11 @@ int check_sprite_texture(sfSprite *sprite, sfTexture *texture)
         return (0);
 }
 
-int check_player_move(moving_t *move)
-{
-    int res = 0;
-
-    if (move == NULL)
-        return (1);
-    res += check_sprite_texture(move->s_right, move->t_right);
-    res += check_sprite_texture(move->s_left, move->t_left);
-    res += check_sprite_texture(move->s_up, move->t_up);
-    res += check_sprite_texture(move->s_down, move->t_down);
-    if (move->rect == NULL)
-        return (1);
-    return (res);
-}
-
 int check_player_asset(player_t *player)
 {
     if (player == NULL || player->stats == NULL || player->stuff == NULL)
         return (1);
-    else if (player->inventory == NULL || check_player_move(player->move) == 1)
+    else if (player->inventory == NULL)
         return (1);
     else
         return (0);
