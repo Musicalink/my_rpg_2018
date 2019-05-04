e163ce030862e9ef1eb8bbb6ea458a96e15844c9
Thomas Crombez <thomas.crombez@epitech.eu>
2019-05-04 15:00:00 +0200

    [~] balancing ork monster

--- src/set/ennemies.c
+++ src/set/ennemies.c
@@ -76,10 +76,10 @@ ebattle_t **gen_enemies(void)
     enemies[1] = gen_bad_guy("ressources/sprites/caillou.png", 396, 480, 11);
     enemies[2] = gen_bad_guy("ressources/sprites/glace.png", 360, 480, 13);
     enemies[3] = gen_bad_guy("ressources/sprites/fighter.png", 211, 480, 18);
-    enemies[0]->stats = set_enemy_stats(90, 200, 10, 20);
-    enemies[1]->stats = set_enemy_stats(110, 30, 15, 20);
-    enemies[2]->stats = set_enemy_stats(160, 30, 40, 20); 
-    enemies[3]->stats = set_enemy_stats(240, 50, 40, 20);
+    enemies[0]->stats = set_enemy_stats(90, 20, 10, 20);
+    enemies[1]->stats = set_enemy_stats(110, 30, 15, 40);
+    enemies[2]->stats = set_enemy_stats(160, 30, 40, 60); 
+    enemies[3]->stats = set_enemy_stats(240, 50, 40, 200);
     set_enemy(enemies[0], (sfVector2f){1050, 200});
     set_enemy(enemies[1], (sfVector2f){1050, 350});
     set_enemy(enemies[2], (sfVector2f){1150, 400});    
