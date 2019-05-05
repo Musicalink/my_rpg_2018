bc58ea73b24d31872e12d835d32038ee6227bfba
Maxence Carpentier <maxence.carpentier@epitech.eu>
2019-05-05 12:25:20 +0200

    [MOD] Minor modification on main.c

--- src/main.c
+++ src/main.c
@@ -14,14 +14,14 @@ int main(int ac, char **av)
     ebattle_t **enemies = gen_enemies();
     sfVideoMode mode = {1920, 1080, 60};
     sfRenderWindow *window;
-    map_t ***maps;
+    map_t ***map;
 
     window = sfRenderWindow_create(mode, "rpg",
         sfClose | sfResize/* | sfFullscreen*/, NULL);
     sfRenderWindow_setFramerateLimit(window, 60);
     srand((unsigned int)time(NULL));
     //   game_menu(menu, window);
-    maps = init_maps();
-    game_map(maps, window, enemies, player);
+    map = init_maps();
+    game_map(map, window, enemies, player);
     return (0);
 }
