ab6197b973d70338464358f11bb8aaf330b2cf93
paul.bryche@epitech.eu <paul.bryche@epitech.eu>
2019-05-05 18:42:15 +0200

    [*]redone pause menu

--- src/pause_menu.c
+++ src/pause_menu.c
@@ -13,13 +13,13 @@ int pause_menu_btn_act(sfRenderWindow *window, menu_t *menu)
     int ret;
 
     if (mouse.x < 1300 && mouse.x > 1000) {
-        if (mouse.y >= 650 && mouse.y < 750) {
+        if (mouse.y >= 650 && mouse.y < 750)
+            return (1);
+        if (mouse.y >= 800 && mouse.y < 900)
+            return (3);
+        if (mouse.y >= 500 && mouse.y < 600)
             return (0);
-        } else if ((mouse.y >= 800 && mouse.y < 900) ||
-            (mouse.y >= 500 && mouse.y < 600)) {
-            ret = (mouse.y >= 800 && mouse.y < 900) ? 1 : 3;
-            return (ret);
-        }
+        return (ret);
     }
     return (2);
 }
@@ -28,19 +28,15 @@ void pause_menu_btn_over(sfRenderWindow *window, menu_t *menu)
 {
     sfVector2i mouse = sfMouse_getPositionRenderWindow(window);
 
-    if (mouse.x < 1500 && mouse.x > 1000) {
-        if (mouse.y >= 650 && mouse.y < 750)
-            sfText_setColor(menu->play, (sfColor){255, 255, 255, 200});
-        else
-            sfText_setColor(menu->play, (sfColor){0, 0, 0, 200});
-        if (mouse.y >= 800 && mouse.y < 900)
-            sfText_setColor(menu->exit, (sfColor){255, 255, 255, 200});
-        else
-            sfText_setColor(menu->exit, (sfColor){0, 0, 0, 200});
-    } else {
-        sfText_setColor(menu->play, (sfColor){0, 0, 0, 200});
-        sfText_setColor(menu->exit, (sfColor){0, 0, 0, 200});
-    }
+    sfText_setColor(menu->play, (sfColor){0, 0, 0, 200});
+    sfText_setColor(menu->exit, (sfColor){0, 0, 0, 200});
+    sfText_setColor(menu->main, (sfColor){0, 0, 0, 200});
+    if (mouse.x < 1500 && mouse.x > 1000 && mouse.y >= 650 && mouse.y < 750)
+        sfText_setColor(menu->play, (sfColor){255, 255, 255, 200});
+    if (mouse.x < 1500 && mouse.x > 1000 && mouse.y >= 800 && mouse.y < 900)
+        sfText_setColor(menu->exit, (sfColor){255, 255, 255, 200});
+    if (mouse.x < 1500 && mouse.x > 1000 && mouse.y >= 500 && mouse.y < 600)
+        sfText_setColor(menu->main, (sfColor){255, 255, 255, 200});
 }
 
 int display_pause_menu(menu_t *menu, sfRenderWindow *window)
@@ -53,7 +49,7 @@ int display_pause_menu(menu_t *menu, sfRenderWindow *window)
             return (1);
         else if (event.type == sfEvtMouseButtonPressed) {
             res = pause_menu_btn_act(window, menu);
-            if (res == 1 || res == 0)
+            if (res == 1 || res == 0 || res == 3)
                 return (res);
         }
     }
@@ -73,7 +69,7 @@ int pause_menu(menu_t *menu, sfRenderWindow *window)
     while (sfRenderWindow_isOpen(window)) {
         res = display_pause_menu(menu, window);
         sfRenderWindow_display(window);
-        if (res == 0 || res == 1)
+        if (res == 0 || res == 1 || res == 3)
             return (res);
     }
     return (1);
